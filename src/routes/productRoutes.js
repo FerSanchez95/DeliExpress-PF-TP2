b403414e91b53d3322aff965cc2497af4a175842
alonsojmateo <alonso.jmateo@gmail.com>
2025-06-25 19:56:34 -0300

    fix product by restaurant id

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import {
     CreateNewProduct,
-    SearchProduct,
     SearchProductById,
     UpdateProduct,
     DeleteProduct,
@@ -11,11 +10,10 @@ import { protegerRuta } from '../../middlewares/authMiddlewares.js';
 
 const router = express.Router();
 
-router.get('/search/products', SearchProduct);
+router.get('/search/products', getProductsByRestaurantId);
 router.get('/search/products/:id', SearchProductById);
 router.post('/products', protegerRuta, CreateNewProduct);
 router.put('/products/:id', protegerRuta, UpdateProduct);
 router.delete('/products/:id', protegerRuta, DeleteProduct);
-router.get('/search/restaurants/:restaurantId/products', getProductsByRestaurantId);
 
 export default router
